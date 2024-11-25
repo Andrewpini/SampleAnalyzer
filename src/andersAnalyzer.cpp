aad1ba7df6ecfafc1eb52d646749e00b9720e335
Anders Storrø <anders.storro@carrier.com>
2024-11-25 12:08:13 +0100

    WIP: Cleaned analyzer code

--- src/andersAnalyzer.cpp
+++ src/andersAnalyzer.cpp
@@ -30,6 +30,11 @@ static bool try_advancing( AnalyzerChannelData* pin )
         pin->Advance( 10000 );
         return true;
     }
+    else if( pin->GetBitState() == BIT_HIGH )
+    {
+        pin->AdvanceToNextEdge();
+        return true;
+    }
     else
     {
         pin->AdvanceToNextEdge();
@@ -40,46 +45,20 @@ static bool try_advancing( AnalyzerChannelData* pin )
 
 void andersAnalyzer::WorkerThread()
 {
-	U64 wrt_start = 0;
-	U64 wrt_end = 0;
+    auto* read_trigger = GetAnalyzerChannelData( mSettings.mReadTriggerChannel );
+    auto* pin1 = GetAnalyzerChannelData( mSettings.mPin1Channel );
+    auto* pin2 = GetAnalyzerChannelData( mSettings.mPin2Channel );
+    auto* pin3 = GetAnalyzerChannelData( mSettings.mPin3Channel );
+    auto* pin4 = GetAnalyzerChannelData( mSettings.mPin4Channel );
 
+    U64 wrt_start = 0;
+    U64 wrt_end = 0;
+    U8 state = 0xff; // Unknown state from start
 
-
-	auto* read_trigger = GetAnalyzerChannelData(mSettings.mReadTriggerChannel);
-	auto* pin1 = GetAnalyzerChannelData(mSettings.mPin1Channel);
-	auto* pin2 = GetAnalyzerChannelData(mSettings.mPin2Channel);
-	auto* pin3 = GetAnalyzerChannelData(mSettings.mPin3Channel);
-	auto* pin4 = GetAnalyzerChannelData(mSettings.mPin4Channel);
-
-	// if( read_trigger->GetBitState() == BIT_LOW )
-	// 	read_trigger->AdvanceToNextEdge();
-
-	// // Unknown state: set it to 0xFF
-	// wrt_end = read_trigger->GetSampleNumber();
-	// Frame frame;
-	// frame.mData1 = 0xff;
-	// frame.mFlags = 0;
-	// frame.mStartingSampleInclusive = 0;
-	// frame.mEndingSampleInclusive = wrt_end;
-	// mResults->AddFrame( frame );
-	// mResults->CommitResults();
-	// ReportProgress( frame.mEndingSampleInclusive );
-	// wrt_start = wrt_end;
-
-
-	bool first_sample_taken;
-	U8 data = 0;
-        U8 state = 0xff;
-
-	/* What do i need:
-		- Sample nr of this trigger rEdge: To be able to read pin data and know start of output
-		- Sample nr of next trigger rEdge: To know end of output
-		- Acumulated pin value*/
-
-	for( ; ; )
-	{
-
-	if (try_advancing(read_trigger)) {
+    for( ;; )
+    {
+        if( try_advancing( read_trigger ) )
+        {
             Frame frame;
             frame.mData1 = state;
             frame.mFlags = 0;
@@ -88,22 +67,23 @@ void andersAnalyzer::WorkerThread()
             mResults->AddFrame( frame );
             mResults->CommitResults();
             ReportProgress( frame.mEndingSampleInclusive );
-	} else {
-		wrt_start = read_trigger->GetSampleNumber();
-		pin1->AdvanceToAbsPosition(read_trigger->GetSampleNumber());
-		pin2->AdvanceToAbsPosition(read_trigger->GetSampleNumber());
-		pin3->AdvanceToAbsPosition(read_trigger->GetSampleNumber());
-		pin4->AdvanceToAbsPosition(read_trigger->GetSampleNumber());
-		state = (pin1->GetBitState() == BIT_HIGH ? 1 : 0) << 0; // Pin 1 (LSB)
-		state |= (pin2->GetBitState() == BIT_HIGH ? 1 : 0) << 1; // Pin 2
-		state |= (pin3->GetBitState() == BIT_HIGH ? 1 : 0) << 2; // Pin 3
-		state |= (pin4->GetBitState() == BIT_HIGH ? 1 : 0) << 3; // Pin 4 (MSB)
+        }
+        else
+        {
+            wrt_start = read_trigger->GetSampleNumber();
+            pin1->AdvanceToAbsPosition( read_trigger->GetSampleNumber() );
+            pin2->AdvanceToAbsPosition( read_trigger->GetSampleNumber() );
+            pin3->AdvanceToAbsPosition( read_trigger->GetSampleNumber() );
+            pin4->AdvanceToAbsPosition( read_trigger->GetSampleNumber() );
+            state = ( pin1->GetBitState() == BIT_HIGH ? 1 : 0 ) << 0;  // Pin 1 (LSB)
+            state |= ( pin2->GetBitState() == BIT_HIGH ? 1 : 0 ) << 1; // Pin 2
+            state |= ( pin3->GetBitState() == BIT_HIGH ? 1 : 0 ) << 2; // Pin 3
+            state |= ( pin4->GetBitState() == BIT_HIGH ? 1 : 0 ) << 3; // Pin 4 (MSB)
 
-		if( read_trigger->GetBitState() == BIT_HIGH )
-		read_trigger->AdvanceToNextEdge();
-
-	}
-	}
+            if( read_trigger->GetBitState() == BIT_HIGH )
+                read_trigger->AdvanceToNextEdge();
+        }
+    }
 }
 
 bool andersAnalyzer::NeedsRerun()
